8e102baf3f060a022640b6737ad5ff990098ea9e
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-06-15 17:09:36 +0300

    Настроил отправку данных в чат

--- controllers/bots.controller.js
+++ controllers/bots.controller.js
@@ -1,23 +1,20 @@
-const bot = require('../bot');
-const chatInfo = require('../chatInfo');
+const TelegramApi = require('node-telegram-bot-api');
+const bot = new TelegramApi(process.env.TOKEN, { polling: true });
 
 module.exports.botsController = {
   sendMessage: async (req, res) => {
     try {
-      if (chatInfo.chatId === +process.env.iqaChatId) {
-        const data = {
-          name: req.body.head_commit.committer.name,
-          commit: req.body.head_commit.message,
-        };
+      const data = {
+        name: req.body.head_commit.committer.name,
+        commit: req.body.head_commit.message,
+      };
 
-        bot.sendMessage(
-          chatInfo.chatId,
-          `${data.name} -> Отправил ПР | Коммит: ${data.commit}`
-        );
+      bot.sendMessage(
+        process.env.iqaChatId,
+        `${data.name} -> Отправил ПР | Коммит: ${data.commit}`
+      );
 
-        return res.json({ message: 'Успешно!' });
-      }
-      return res.json({ message: 'Введите тег в чат для запуска бота!' });
+      return res.json({ message: 'Успешно!' });
     } catch (e) {
       return res.status(400).json({ error: e.toString() });
     }
