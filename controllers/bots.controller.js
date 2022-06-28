ae2f600b65a8102188e9bcbfa681f172087e4961
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-06-28 09:39:50 +0300

    Доработал бота. Теперь приходят уведомление при форке и ПР

--- controllers/bots.controller.js
+++ controllers/bots.controller.js
@@ -2,19 +2,40 @@ const TelegramApi = require('node-telegram-bot-api');
 const bot = new TelegramApi(process.env.TOKEN, { polling: true });
 
 module.exports.botsController = {
-  sendMessage: async (req, res) => {
+  sendMessage: (req, res) => {
     try {
-      const data = {
-        name: req.body.head_commit.committer.name,
-        commit: req.body.head_commit.message,
-      };
-
-      bot.sendMessage(
-        process.env.iqaChatId,
-        `${data.name} -> Отправил ПР | Коммит: ${data.commit}`
-      );
-
-      return res.json({ message: 'Успешно!' });
+      // Если произошел fork
+      if (req.body.forkee) {
+        bot.sendMessage(
+          process.env.iqaChatId,
+          `${req.body.forkee.owner.login} сделал форк репозитория`
+        );
+        return res.json({ message: 'Успешно!' });
+      }
+      // Если произошло слияние ветки с main
+      if (req.body.head_commit?.committer.name === 'GitHub') {
+        bot.sendMessage(
+          process.env.iqaChatId,
+          `Произошло слияние ветки с main`
+        );
+        return res.json({ message: 'Успешно!' });
+      }
+      // Открыл ПР
+      if (req.body.action === 'opened') {
+        bot.sendMessage(
+          process.env.iqaChatId,
+          `${req.body.pull_request.user.login} => Отправил ПР`
+        );
+        return res.json({ message: 'Успешно!' });
+      }
+      // Обновил ПР
+      if (req.body.action === 'synchronize') {
+        bot.sendMessage(
+          process.env.iqaChatId,
+          `${req.body.pull_request.user.login} => Обновил ПР`
+        );
+        return res.json({ message: 'Успешно!' });
+      }
     } catch (e) {
       return res.status(400).json({ error: e.toString() });
     }
